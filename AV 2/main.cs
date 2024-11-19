4b24c2da7b3866125b65f0b1273110dbb9374ace
yasmintds <162698196+yasmintds@users.noreply.github.com>
2024-11-19 11:56:39 -0300

    Add files via upload

---
+++ AV 2/main.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+
+    String siga = "verde";
+    if(siga == "verde"){
+      Console.WriteLine("Pode passar");
+    }else if(siga == "amarelo"){
+      Console.WriteLine("Atenção");
+    }else if(siga == "vermelho"){
+      Console.WriteLine("Pare");
+
+      }
+
+  
+    
+  }
+}
